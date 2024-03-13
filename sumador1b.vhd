8c59459e3cd7b2bc68a6b9b0714122c1f3068961
Flavio Miravete <flavio.miravete@gmail.com>
2024-03-13 20:09:21 -0300

    new file:   sumador1b.vhd

---
+++ sumador1b.vhd
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+-- ****************************** --
+-- Sumador de 1 bit --
+-- ****************************** --
+entity sum1b is
+    port ( op1: in std_logic; 
+           op2: in std_logic; 
+            ci: in std_logic;
+           sal: out std_logic;
+            co: out std_logic
+    );
+end sum1b
+
+architecture sum1b_arch of sum1b is
+begin
+    sal <= op1 xor op2 xor ci;
+    co <= (op1 and op2) or (op1 and ci) or (op2 and ci);
+end;
