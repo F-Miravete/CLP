e97d9df60ee0dc58a2bc6f51bda014b6fdae9398
Flavio Miravete <flavio.miravete@gmail.com>
2024-03-18 19:08:57 -0300

    Ejercicio 1 Sumador 1b

--- sumador1b.vhd
+++ sumador1b.vhd
@@ -13,10 +13,10 @@ entity sum1b is
     sal : out std_logic;
     co  : out std_logic
   );
-end sum1b
+end sum1b;
 
 architecture sum1b_arch of sum1b is
 begin
   sal <= op1 xor op2 xor ci;
   co  <= (op1 and op2) or (op1 and ci) or (op2 and ci);
-end;
+end sum1b_arch;
