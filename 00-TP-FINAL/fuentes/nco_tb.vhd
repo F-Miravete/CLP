625b33ab3d13b76467dcaa8ed89ab31ec22a05b6
Flavio Miravete <flavio.miravete@gmail.com>
2024-04-05 21:13:55 -0300

    Agrego simulacion

--- 00-TP-FINAL/fuentes/nco_tb.vhd
+++ 00-TP-FINAL/fuentes/nco_tb.vhd
@@ -18,17 +18,18 @@ architecture nco8_tb_arq of nco8_tb is
 	end component;
 
     -- Declaracion de senales de prueba
-	signal i_clk_tb: std_logic ='0';
-	signal i_rstb_tb: std_logic = '0';
+	signal i_clk_tb: std_logic :='0';
+	signal i_rstb_tb: std_logic := '1';
 	signal i_sync_reset_tb: std_logic := '1';
-	signal i_fcw_tb: std_logic_vector(7 downto 0) = "00000100";
+	signal i_fcw_tb: std_logic_vector(7 downto 0) := "00001010";
 	signal o_nco_tb: std_logic_vector(7 downto 0);
 
 begin
 
     i_clk_tb <= not i_clk_tb after 5 ns; 
     i_sync_reset_tb <= '0' after 20 ns;
-
+    i_fcw_tb <= "00100000" after 1000 ns;
+    
     DUT: nco8
 		port map(
 			i_clk	=> i_clk_tb, 
